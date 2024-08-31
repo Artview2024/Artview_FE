4f5c3a9b7a194b8f7c60ceed8f6605eba1839b08
lse415 <leesuel0379@gmail.com>
2024-09-01 03:52:56 +0900

    chore

--- src/utils/fileNameUtils.tsx
+++
@@ -1,7 +0,0 @@
-import uuid from 'react-native-uuid';
-
-export const getShortFileName = (originalName: string): string => {
-  const extension = originalName.split('.').pop();
-  const shortName = uuid.v4();
-  return `${shortName}.${extension}`;
-};
